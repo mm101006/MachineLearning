4a6b21c658340de6ed64ff26241a02e883322923
Matthew Mangiaracina <mm101006@gmail.com>
2018-12-31 20:57:30 -0500

    adds additional feats

--- plinko/score.js
+++ plinko/score.js
@@ -1,32 +1,54 @@
 const outputs = [];
-const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
-function distance(pointA, pointB) {
-  return Math.abs(pointA - pointB);
-}
 
 function runAnalysis() {
-  const bucket = knn();
+  const testSetSize = 100;
+  const k = 10;
 
-  console.log('Your point will probably fall into', bucket);
+  _.range(0, 3).forEach(feature => {
+    const data = _.map(outputs, row => [row[feature], _.last(row)]);
+    const [testSet, trainingSet] = splitDataset(minMax(data, 1), testSetSize);  
+    const accuracy = _.chain(testSet)
+    .filter(testPoint => knn(trainingSet, _.initial(testPoint), k) === _.last(testPoint))
+    .size()
+    .divide(testSetSize)
+    .value();
+
+    console.log('For feature of', feature, 'accuracy is', accuracy);
+  });
 }
 
-function knn(data) {
-  return  _.chain(data, point)
-    .map(row => [distance(row[0], point), row[3]])
-    .sortBy(row => row[0])
-    .slice(0,k)
-    .countBy(row => row[1])
-    .toPairs()
-    .sortBy(row => row[1])
-    .last()
-    .first()
-    .parseInt()
-    .value()
+function knn(data, point, k) {
+  return  _.chain(data)
+    .map(row => {
+      return [
+        distance(_.initial(row), point), 
+        _.last(row)
+      ];
+    })
+      .sortBy(row => row[0])
+      .slice(0,k)
+      .countBy(row => row[1])
+      .toPairs()
+      .sortBy(row => row[1])
+      .last()
+      .first()
+      .parseInt()
+      .value()
+    }
+
+function distance(pointA, pointB) {
+  return (
+    _.chain(pointA)
+      .zip(pointB)
+      .map(([a, b]) => (a - b) ** 2)
+      .sum()
+      .value() ** 0.5
+  );
 }
 
 function splitDataset(data, testCount) {
@@ -36,4 +58,21 @@ function splitDataset(data, testCount) {
   const trainingSet = _.slice(shuffled, testCount);
 
   return [testSet, trainingSet];
+}
+
+function minMax(data, featureCount) {
+  const clonedData = _.cloneDeep(data);
+
+  for (let i = 0; i < featureCount; i++) {
+    const column = clonedData.map(row => row[i]);
+
+    const min = _.min(column);
+    const max = _.max(column);
+
+    for (let j = 0; j < clonedData.length; j++){
+      clonedData[j][i] = (clonedData[j][i] - min) / (max -min);
+    }
+  }
+
+  return clonedData;
 }
